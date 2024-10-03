18f522898370184e2f1855a17f6be0386c5105c1
taqiussa <taqiussa@gmail.com>
2024-10-03 07:04:07 +0700

    data santri controller

--- app/Http/Controllers/Sekretaris/DataSantriController.php
+++ app/Http/Controllers/Sekretaris/DataSantriController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Sekretaris;
 use App\Http\Controllers\Controller;
 use App\InitTrait;
 use App\Models\Santri;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DataSantriController extends Controller
@@ -13,13 +14,26 @@ class DataSantriController extends Controller
 
     public function index()
     {
+        $nis = [];
+
+        if (request('search') != '' || request('search') != null) {
+            $nis = User::query()
+                ->whereNotNull('nis')
+                ->where('name', 'like', '%' . request('search') . '%')
+                ->pluck('nis');
+        }
+
         return inertia('Sekretaris/DataSantri', [
             'initTahun' => $this->data_tahun(),
             'listSantri' => Santri::query()
                 ->whereTahun(request('tahun') ?? $this->data_tahun())
                 ->when(
                     request('jenis_kelamin'),
-                    fn($q) => $q->whereJenisKelamin
+                    fn($q) => $q->whereJenisKelamin(request('jenis_kelamin'))
+                )
+                ->when(
+                    request('search'),
+                    fn($q) => $q->whereIn('nis', $nis)
                 )
                 ->with(['biodata', 'santri:nis,name'])
                 ->paginate(10)
