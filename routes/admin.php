78fd093870fb2cc6c5dc70884bc88aed68285447
taqiussa <taqiussa@gmail.com>
2024-10-16 08:44:17 +0700

    atur role

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Admin\AturRoleController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->group(function () {
-
+        Route::controller(AturRoleController::class)->group(function () {
+                Route::get('atur-role', 'index')->name('atur-role');
+                Route::post('atur-role', 'simpan')->name('atur-role.simpan');
+                Route::delete('atur-role', 'hapus')->name('atur-role.hapus');
+        });
 });
